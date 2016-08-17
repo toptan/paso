dbabea41e1916bfaadc8dd795665261bd5732d7e
Toplica Tanasković <toplicius@gmail.com>
2016-08-17 10:55:37 +0200

    Fixing coverage source paths. Again...

--- tests/setup_test.sh
+++ tests/setup_test.sh
@@ -7,6 +7,6 @@ docker logs $DOCKER_CONTAINER_ID
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -c "bash -xe /paso/tests/test_inside_docker.sh"
 docker ps -a
 # Fix coverage report paths
-cat combined-coverage.info | sed "s|/paso/|`pwd`/paso/|g" > coverage.info
+cat combined-coverage.info | sed "s|/paso/|`pwd`/|g" > coverage.info
 docker stop $DOCKER_CONTAINER_ID
 docker rm -v $DOCKER_CONTAINER_ID
