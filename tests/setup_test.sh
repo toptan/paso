3811cbad78cec4250e525c4a3ce2e0095744fdd3
Toplica Tanasković <toplicius@gmail.com>
2016-08-20 21:43:55 +0200

    Added CentOS 7 to CI build matrix

--- tests/setup_test.sh
+++ tests/setup_test.sh
@@ -1,10 +1,25 @@
 #!/bin/sh -xe
 # This script starts docker and runs tests.
 
-docker run --privileged -d -ti -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/paso:rw ubuntu:xenial bash
-DOCKER_CONTAINER_ID=$(docker ps | grep ubuntu | awk '{print $1}')
+case "$1" in
+        ubuntu)
+            docker run --privileged -d -ti -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/paso:rw ubuntu:xenial bash
+            DOCKER_CONTAINER_ID=$(docker ps | grep ubuntu | awk '{print $1}')
+            ;;
+
+        centos)
+            docker run --privileged -d -ti -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/paso:rw centos:centos7 bash
+            DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+            ;;
+
+        *)
+            echo "Unsuported linux distribution $1. Only Ubuntu 16.04 and CentOS 7 are supported."
+            exit 1
+
+esac
+
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -c "bash -xe /paso/tests/test_inside_docker.sh"
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -c "bash -xe /paso/tests/test_inside_docker.sh $1"
 docker ps -a
 # Fix coverage report paths
 cat combined-coverage.info | sed "s|/paso/|`pwd`/|g" > coverage.info
--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -1,16 +1,60 @@
 #!/bin/sh -xe
 
-# Update everything
-apt --yes update
-apt --yes dist-upgrade
+case "$1" in
+        ubuntu)
+            # Update everything
+            apt --yes update
+            apt --yes dist-upgrade
 
-# Install needed packages
-apt --yes install gcc g++ libboost-dev cmake git tar gzip make python cppcheck \
-          doxygen lcov gcovr graphviz qttools5-dev-tools qt5-default psmisc \
-          mwm xauth sudo xvfb libqt5sql5-sqlite
+            # Install needed packages
+            apt --yes install gcc g++ libboost-dev cmake git tar gzip make python cppcheck \
+                      doxygen lcov gcovr graphviz qttools5-dev-tools qt5-default psmisc \
+                      mwm xauth sudo xvfb libqt5sql5-sqlite
 
-# Generate and set locale
-locale-gen en_US.UTF-8
+            # Generate locale
+            locale-gen en_US.UTF-8
+
+            # Set cmake command
+            CMAKE_COMMAND=cmake
+            ;;
+
+        centos)
+            # Clean all caches
+            yum -y clean all
+            yum -y clean expire-cache
+
+            # Enable EPEL
+            yum -y install epel-release
+
+            # An ugly hack as check-update exit code is 100 when there are packages to update
+            echo `yum -y check-update`
+
+            # Upgrade
+            yum -y upgrade
+
+            # Install needed packages
+            yum -y install rpm-build gcc gcc-c++ boost-devel cmake3 git tar gzip make \
+                   python cppcheck doxygen lcov python-pip graphviz qt5-linguist \
+                   qt5-qtbase-devel qt5-qtbase-gui xorg-x11-server-Xvfb sysvinit-tools \
+                   glibc-static psmisc motif which xauth
+
+            # Install gcovr
+            pip install gcovr==3.2
+
+            # Fix dbus inside docker
+            dbus-uuidgen > /etc/machine-id
+
+            # Set cmake command
+            CMAKE_COMMAND=cmake3
+            ;;
+
+        *)
+            echo $"Unsupported linux distribution $1. Only Ubuntu 16.04 and CentOS 7 are supported."
+            exit 1
+
+esac
+
+# Set locale
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
@@ -18,7 +62,7 @@ export LC_ALL=en_US.UTF-8
 # Prepare dirs and configure build
 mkdir -p /tmp/build
 cd /tmp/build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ENABLE_COVERAGE=ON /paso
+$CMAKE_COMMAND -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ENABLE_COVERAGE=ON /paso
 
 # Detect number of CPUs online
 CPU_COUNT="$(getconf _NPROCESSORS_ONLN | tr -d '\n')"
